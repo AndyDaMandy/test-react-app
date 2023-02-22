521fbdd0446964adb5887707ad763c0d89e8b748
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-02-22 09:06:26 -0500

    setting up state in squares

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
     }
     render() {
       return (
-        <button className="square" onClick={() => console.log('click')}>
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
           {this.props.value}
         </button>
       );
